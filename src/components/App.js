7bfe3921602f416e99a202ed446324f1f1570bcd
AnaghaPatil189 <109735323+AnaghaPatil189@users.noreply.github.com>
2022-12-04 19:42:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,3 @@
-// import React from 'react'
-// import '../styles/App.css';
-// const App = () => {
-//   const submitHandler = (e)=>{
-//     e.preventDefault();
-//     console.log("form submitted");
-//   }
-
-//   return (
-//     <div id="main">
-//       <form onClick={submitHandler}>
-//         <label htmlFor='name'>Name</label>
-//         <input id="name" type={"text"} />
-
-//         <br />
-//         <br />
-//         <button type='submit'>Submit</button>
-//       </form>
-//     </div>
-//   )
-// }
-
-
-export default App;
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
